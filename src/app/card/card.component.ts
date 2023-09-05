c3db17b254c10d993b61a8ab45d86d27b53ef34e
MrSharma619 <sh.manthan99@gmail.com>
2023-09-05 23:17:02 +0530

    second panel finished

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,21 +1,56 @@
 import { Component } from '@angular/core';
 import { ScoreService } from '../services/score.service';
 
+interface Subject {
+  category: string;
+  score: number;
+  icon: string;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-  overallScore: number;
+  overallScore: number = 0;
   data: any;
+  reaction: Subject | undefined;
+  memory: Subject | undefined;
+  verbal: Subject | undefined;
+  visual: Subject | undefined;
 
-  constructor(scoreServ: ScoreService){
-    this.data = scoreServ.getData();
-    this.overallScore = this.calculateScore();
+  constructor(scoreServ: ScoreService) {
+    scoreServ.getData().then((res) => {
+      //console.log(res);
+
+      this.data = res;
+
+      this.overallScore = Math.round(this.calculateScore());
+
+      this.reaction = this.data[0];
+
+      this.memory = this.data[1];
+
+      this.verbal = this.data[2];
+
+      this.visual = this.data[3];
+
+      //console.log(this.data);
+    });
   }
 
-  calculateScore(){
-    return 76;
+  calculateScore() {
+    //console.log(this.data);
+
+    const sum = this.data
+      .map((element: any) => element.score)
+      .reduce((a: number, b: number) => a + b, 0);
+
+    const average = sum / this.data.length;
+
+    //console.log(average);
+
+    return average;
   }
 }
--- src/app/services/score.service.ts
+++ src/app/services/score.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScoreService {
+  constructor() {}
 
-  constructor() { }
+  getData() {
+    const url = 'assets/data.json';
 
-  getData(){
-    return null;
+    const fetchJson = async () => {
+      try {
+        const data = await fetch(url);
+        const response = await data.json();
+
+        //console.log(response);
+
+        return response;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    return fetchJson();
   }
 }
